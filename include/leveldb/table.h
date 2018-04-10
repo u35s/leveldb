c58627b2cae9c4f6b61a7fbcc9a8125b9972346e
u35s <zjt@mail.com>
2018-04-10 10:57:48 +0800

    table 中文注释

--- include/leveldb/table.h
+++ include/leveldb/table.h
@@ -19,14 +19,25 @@ class RandomAccessFile;
 struct ReadOptions;
 class TableCache;
 
+// 表是一个从字符串到字符串的排序映射。
+// 表是不可变的和持久的。
+// 可以从多个线程安全地访问一个表，而不需要外部同步。
+//
 // A Table is a sorted map from strings to strings.  Tables are
 // immutable and persistent.  A Table may be safely accessed from
 // multiple threads without external synchronization.
 class LEVELDB_EXPORT Table {
  public:
+  // 尝试打开存储在"file"中的字节[0..file_size)的表,
+  // 并读取允许从表中检索数据所需的元数据项。
+  //
   // Attempt to open the table that is stored in bytes [0..file_size)
   // of "file", and read the metadata entries necessary to allow
   // retrieving data from the table.
+  //
+  // 如果成功,返回ok并将"*table"设置为新打开的表。当不再需要时，客户端应该删除"*table"。
+  // 如果在初始化表时出现错误,则将"*table"设置为NULL,并返回一个不确定的状态。
+  // 不接受"*sourve"的所有权，但客户必须确保"源"仍然存活在返回表的生命周期内。
   //
   // If successful, returns ok and sets "*table" to the newly opened
   // table.  The client should delete "*table" when no longer needed.
@@ -35,6 +46,7 @@ class LEVELDB_EXPORT Table {
   // "*source", but the client must ensure that "source" remains live
   // for the duration of the returned table's lifetime.
   //
+  // *文件在使用时必须保持有效。
   // *file must remain live while this Table is in use.
   static Status Open(const Options& options,
                      RandomAccessFile* file,
@@ -51,6 +63,11 @@ class LEVELDB_EXPORT Table {
   // call one of the Seek methods on the iterator before using it).
   Iterator* NewIterator(const ReadOptions&) const;
 
+  // 给定一个键,在文件中返回一个近似的字节偏移量,
+  // 在该文件中,该键的数据开始(如果文件中有关键字，就会开始)。
+  // 返回的值以文件字节为单位，因此包括对底层数据的压缩等效果。
+  // 例如，表中最后一个键的近似偏移量将接近文件长度。
+  //
   // Given a key, return an approximate byte offset in the file where
   // the data for that key begins (or would begin if the key were
   // present in the file).  The returned value is in terms of file
